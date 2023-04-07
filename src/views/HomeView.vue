0640264639a20830e086e462ece211992c987c6c
Evandro Mathias Friedrichsen <evandrofriedri@gmail.com>
2023-04-07 09:38:07 -0300

    Instalado vuejs e tailwindcss

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,7 +1,13 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
+    <!-- <img alt="Vue logo" src="../assets/logo.png"> -->
     <HelloWorld msg="Welcome to Your Vue.js App"/>
+  </div>
+  <div>
+  <h1 class="mb-4 text-3xl font-bold text-blue-200 ">
+    Tailwind CSS 3 with vue js 3
+  </h1>
+  <button class="px-6 py-2 text-sm text-green-100 bg-indigo-600 rounded">Next</button>
   </div>
 </template>
 
