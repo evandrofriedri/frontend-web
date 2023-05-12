4c31e23733e87ec8fb55c5bce0b7eefbb2d4c64b
Evandro Mathias Friedrichsen <evandrofriedri@gmail.com>
2023-05-12 13:01:19 -0300

    Instalado Vuelidate e criado validacao na tela de cadastro

--- src/main.js
+++ src/main.js
@@ -4,11 +4,13 @@ import { library } from '@fortawesome/fontawesome-svg-core';
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
 import { faUser } from '@fortawesome/free-regular-svg-icons';
 import { faBars, faXmark } from '@fortawesome/free-solid-svg-icons';
-import App from './App.vue';
 import router from './router';
 import './index.css';
+import App from './App.vue';
+
 // eslint-disable-next-line import/no-relative-packages
 import '../node_modules/nprogress/nprogress.css';
+
 import DefaultLayout from './components/layouts/DefaultLayout.vue';
 import EmptyLayout from './components/layouts/EmptyLayout.vue';
 
--- src/views/create-account/CreateAccountView.vue
+++ src/views/create-account/CreateAccountView.vue
@@ -9,36 +9,80 @@
       <div class="mx-auto w-full max-w-sm">
         <div class="p-5 bg-white sm:shadow-2xl sm:rounded-xl">
           <img src="../../assets/undraw_sign_up_n6im.svg" alt="" class="lg:hidden h-20 w-auto">
-          <h2 class="mt-6 text-3xl text-center font-semibold text-gray-700">
+          <h2 class="mt-6 mb-6 text-3xl text-center font-semibold text-gray-700">
             Criar seu cadastro
           </h2>
           <form action="" @submit.prevent="submitForm">
             <div class="mb-4">
-              <p class="mt-2 text-sm text-gray-700 max-w">
-                * Nome Completo
-              </p>
-              <label><input v-model.trim="account.name" class="appearance-none block w-full py-3 px-4 leading-tight text-gray-700 bg-gray-50 focus:bg-white borde border-gray-200 rounded-md focus:outline-none" type="text" placeholder="Insira seu Nome" /></label>
+              <label class="text-base text-gray-700 max-w"> Nome Completo
+                <input
+                  id="username2"
+                  v-model.trim="formData.userName"
+                  name="name"
+                  class="appearance-none block w-full py-3 px-4 leading-tight text-gray-700 bg-gray-50 focus:bg-white borde border-gray-200 rounded-md focus:outline-none"
+                  type="text"
+                />
+              </label>
+              <span v-for="error in v$.userName.$errors" :key="error.$uid" class="text-sm text-red-600">
+                {{ error.$message }}
+              </span>
             </div>
             <div class="mb-4">
-              <p class="mt-2 text-sm text-gray-700 max-w">
-                * Email
+              <p class="text-base text-gray-700 max-w">
+                Email
               </p>
-              <label><input v-model.trim="account.email" class="appearance-none block w-full py-3 px-4 leading-tight text-gray-700 bg-gray-50 focus:bg-white borde border-gray-200 rounded-md focus:outline-none" type="text" placeholder="Insira seu Email" /></label>
+              <label>
+                <input
+                  id="userEmail2"
+                  v-model.trim="formData.userEmail"
+                  name="email"
+                  class="appearance-none block w-full py-3 px-4 leading-tight text-gray-700 bg-gray-50 focus:bg-white borde border-gray-200 rounded-md focus:outline-none"
+                  type="text"
+                />
+              </label>
+              <span v-for="error in v$.userEmail.$errors" :key="error.$uid" class="text-sm text-red-600">
+                {{ error.$message }}
+              </span>
             </div>
             <div class="mb-4">
-              <p class="mt-2 text-sm text-gray-700 max-w">
-                * Senha
+              <p class="mt-3 text-base text-gray-700 max-w">
+                Senha
               </p>
-              <label><input v-model="account.password" class="appearance-none block w-full py-3 px-4 leading-tight text-gray-700 bg-gray-50 focus:bg-white borde border-gray-200 rounded-md focus:outline-none" type="password" placeholder="Insira sua Senha"></label>
+              <label>
+                <input
+                  id="password"
+                  v-model="formData.password"
+                  name="password"
+                  class="appearance-none block w-full py-3 px-4 leading-tight text-gray-700 bg-gray-50 focus:bg-white borde border-gray-200 rounded-md focus:outline-none"
+                  type="password"
+                />
+              </label>
+              <span v-for="error in v$.password.$errors" :key="error.$uid" class="text-sm text-red-600">
+                {{ error.$message }}
+              </span>
             </div>
             <div class="mb-4">
-              <p class="mt-2 text-sm text-gray-700 max-w">
-                * Repita a Senha
+              <p class="mt-3 text-base text-gray-700 max-w">
+                Insira novamente a Senha
               </p>
-              <label><input v-model="account.password" class="appearance-none block w-full py-3 px-4 leading-tight text-gray-700 bg-gray-50 focus:bg-white borde border-gray-200 rounded-md focus:outline-none" type="password" placeholder="Insira sua Senha novamente"></label>
+              <label>
+                <input
+                  id="confirmPassword"
+                  v-model="formData.confirmPassword"
+                  name="confirmPassword"
+                  class="appearance-none block w-full py-3 px-4 leading-tight text-gray-700 bg-gray-50 focus:bg-white borde border-gray-200 rounded-md focus:outline-none"
+                  type="password"
+                />
+              </label>
+              <span v-for="error in v$.confirmPassword.$errors" :key="error.$uid" class="text-sm text-red-600">
+                {{ error.$message }}<br>
+              </span>
             </div>
             <div class="mb-4">
-              <button class="inline-block w-full py-4 px-8 leading-none text-white bg-blue-700 hover:bg-blue-900 font-semibold rounded-md" type="submit">
+              <button
+                class="inline-block w-full py-4 px-8 leading-none text-white bg-blue-700 hover:bg-blue-900 font-semibold rounded-md"
+                type="submit"
+              >
                 Cadastrar
               </button>
             </div>
@@ -49,21 +93,46 @@
   </div>
 </template>
 
-<script>
+<script setup>
+import { useVuelidate } from '@vuelidate/core';
+import {
+  required, email, sameAs, minLength, helpers,
+} from '@vuelidate/validators';
+import { reactive, computed } from 'vue';
 
-export default {
-  name: 'CreateAccount',
-  data() {
-    return {
-      account: {
-        name: '',
-        email: '',
-        password: '',
-      },
-    };
+const formData = reactive({
+  userName: '',
+  userEmail: '',
+  password: '',
+  confirmPassword: '',
+});
+
+const rules = computed(() => ({
+  userName: { required: helpers.withMessage('Campo obrigatório', required) },
+  userEmail: {
+    required: helpers.withMessage('Campo obrigatório', required),
+    email: helpers.withMessage('Insira um email válido', email),
   },
-  methods: {
-    submitForm() { },
+  password: {
+    required: helpers.withMessage('Campo obrigatório', required),
+    minLength: helpers.withMessage('Mínimo 8 caracteres', minLength(8)),
   },
+  confirmPassword: {
+    required: helpers.withMessage('Campo obrigatório ', required),
+    sameAs: helpers.withMessage('As senhas não são iguais', sameAs(formData.password)),
+  },
+}));
+
+const v$ = useVuelidate(rules, formData);
+
+const submitForm = async () => {
+  const result = await v$.value.$validate();
+
+  if (result) {
+    console.log('fomr enviado!');
+  } else {
+    console.log('nao enviado!');
+  }
 };
+
 </script>
