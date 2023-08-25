1dd85fff8249b26e6ef754797f49485174a302fe
Evandro Mathias Friedrichsen <evandrofriedri@gmail.com>
2023-08-25 14:48:30 -0300

    Ajustado library sweetalert

--- src/main.js
+++ src/main.js
@@ -2,7 +2,7 @@ import { createApp } from 'vue';
 
 import { library } from '@fortawesome/fontawesome-svg-core';
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
-import VueSweetalert2 from 'vue-sweetalert2';
+// import VueSweetalert2 from 'vue-sweetalert2';
 
 import './index.css';
 import mitt from 'mitt';
@@ -12,6 +12,7 @@ import icons from './assets/icons';
 
 // eslint-disable-next-line import/no-relative-packages
 import '../node_modules/nprogress/nprogress.css';
+import 'sweetalert2/dist/sweetalert2.min.css';
 
 import DefaultLayout from './layouts/DefaultLayout.vue';
 import EmptyLayout from './layouts/EmptyLayout.vue';
@@ -21,7 +22,7 @@ library.add({ ...icons });
 const emitter = mitt();
 
 createApp(App)
-  .use(router, VueSweetalert2)
+  .use(router)
   .component('font-awesome-icon', FontAwesomeIcon)
   .component('default-layout', DefaultLayout)
   .component('empty-layout', EmptyLayout)
