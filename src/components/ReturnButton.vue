b01ed3860971fe2c633b59149383e2888bf7ba6a
Evandro Mathias Friedrichsen <evandrofriedri@gmail.com>
2023-09-08 11:35:37 -0300

    Criado Componente ReturnButton

---
+++ src/components/ReturnButton.vue
@@ -0,0 +1,17 @@
+<template>
+  <button class="mr-4" type="button">
+    <font-awesome-icon icon="fa-solid fa-chevron-left" size="lg" />
+  </button>
+</template>
+<script setup>
+defineProps({
+  btnType: {
+    type: String,
+    default: 'button',
+  },
+  description: {
+    type: String,
+    default: '',
+  },
+});
+</script>
