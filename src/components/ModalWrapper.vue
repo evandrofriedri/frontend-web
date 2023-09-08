1c6db5bc4ca7e4cffb35bf61fe62924b5c5d7c10
Evandro Mathias Friedrichsen <evandrofriedri@gmail.com>
2023-09-08 10:53:40 -0300

    Criado componente ModalWrapper

---
+++ src/components/ModalWrapper.vue
@@ -0,0 +1,45 @@
+<template>
+  <Teleport to="#modal">
+    <Transition name="modal">
+      <div v-if="isModalOpen" class="flex fixed top-0 left-0 z-10 w-screen h-screen bg-black/50 justify-center items-center">
+        <div ref="modal" class="relative p-4 mx-auto w-screen h-screen sm:h-auto md:max-w-md lg:max-w-lg bg-white shadow-2xl rounded">
+          <slot />
+        </div>
+      </div>
+    </Transition>
+  </Teleport>
+</template>
+<script setup>
+import { onUpdated, ref, inject } from 'vue';
+import { onClickOutside } from '@vueuse/core';
+
+const props = defineProps({
+  modalOpen: {
+    type: Boolean,
+    default: false,
+  },
+});
+
+const isModalOpen = ref(false);
+const modal = ref(null);
+const emitter = inject('emitter');
+
+onClickOutside(modal, () => {
+  isModalOpen.value = false;
+  emitter.emit('setModalFalse');
+  console.log('clicouu');
+});
+
+emitter.on('closeModal', () => {
+  console.log('chegou no closemodal');
+  isModalOpen.value = false;
+  emitter.emit('setModalFalse');
+});
+
+onUpdated(async () => {
+  console.log(props.modalOpen);
+  console.log(isModalOpen.value);
+  isModalOpen.value = await props.modalOpen;
+});
+
+</script>
