1ca374552dd3994fc8cc534ce345e2668debacdc
Evandro Mathias Friedrichsen <evandrofriedri@gmail.com>
2023-12-05 17:13:53 -0300

    Teste baseURL

--- src/services/Api.js
+++ src/services/Api.js
@@ -3,7 +3,8 @@
 
 import axios from 'axios';
 export default () => axios.create({
-  baseURL: process.env.VUE_APP_API_URL,
+  // baseURL: process.env.VUE_APP_API_URL,
+  baseURL: 'https://deliveryapi.vercel.app/api',
   headers: {
     Authorization: `Bearer ${localStorage.getItem('jwt')}`,
   },
