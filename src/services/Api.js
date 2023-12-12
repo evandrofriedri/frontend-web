d0c5d1775a86c2afd9ef648f828f9739fdcaa8b4
Evandro Mathias Friedrichsen <evandrofriedri@gmail.com>
2023-12-12 15:21:39 -0300

    Comentado APi service

--- src/services/Api.js
+++ src/services/Api.js
@@ -1,10 +1,10 @@
 /* Responsavel por inicializar o axios e as requisições da url do HTTPS
 */
 
-import axios from 'axios';
-export default () => axios.create({
-  baseURL: process.env.VUE_APP_API_URL,
-  headers: {
-    Authorization: `Bearer ${localStorage.getItem('jwt')}`,
-  },
-});
+// import axios from 'axios';
+// export default () => axios.create({
+//   baseURL: process.env.VUE_APP_API_URL,
+//   headers: {
+//     Authorization: `Bearer ${localStorage.getItem('jwt')}`,
+//   },
+// });
