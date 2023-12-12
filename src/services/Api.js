7349040fe3f713dbd256c5d2b0693bbc985c5c17
Evandro Mathias Friedrichsen <evandrofriedri@gmail.com>
2023-12-12 20:25:36 -0300

    Adicionado baseURL

--- src/services/Api.js
+++ src/services/Api.js
@@ -2,9 +2,8 @@
 */
 
 import axios from 'axios';
-console.log('PASSSOU NO AXIOSS');
 export default () => axios.create({
-  baseURL: process.env.VUE_APP_API_URL,
+  baseURL: 'https://deliveryapi.vercel.app/api',
   headers: {
     Authorization: `Bearer ${localStorage.getItem('jwt')}`,
   },
